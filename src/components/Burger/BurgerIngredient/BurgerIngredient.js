565cbdec2a1bb6d541ab977b5c07247ab6f631ef
Jameson Campbell <jcampbell@flexterapp.com>
2020-08-27 15:34:27 -0500

    displaying burger component on screen

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -13,8 +13,8 @@ class BurgerIngredient extends Component {
             case ('bread-top'):
                 ingredient = (
                     <div className={classes.BreadTop}>
-                        <div> className={classes.Seeds1}</div>
-                        <div> className={classes.Seeds1}</div>
+                        <div className={classes.Seeds1}></div>
+                        <div className={classes.Seeds2}></div>
                     </div>
                 );
                 break;
@@ -39,7 +39,7 @@ class BurgerIngredient extends Component {
 
 BurgerIngredient.propTypes = {
     type: PropTypes.string.isRequired
-}
+} 
 
 export default BurgerIngredient;
 
