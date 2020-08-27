d4b933a5b0af7646a5e13963795bcb1d8b015171
Jameson Campbell <jcampbell@flexterapp.com>
2020-08-27 17:38:38 -0500

    totalPrice state when adding ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import React, { Component, Fragment } from 'react';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.6
+}
+
 class BurgerBuilder extends Component {
 
     state = {
@@ -11,7 +18,8 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4
     }
 
     addIngredientHandler = type => {
@@ -20,15 +28,20 @@ class BurgerBuilder extends Component {
         const updatedIngredients = {
             ...this.state.ingredients
         }
-        updatedIngredients[type] = updatedCount
-        this.setState({ ingredients: updatedIngredients })
+        updatedIngredients[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+
+        this.setState({ ingredients: updatedIngredients, totalPrice: newPrice })
     }
 
     render() {
+        {console.log(this.state.totalPrice)}
         return (
             <Fragment>
-                <Burger ingredients={this.state.ingredients}/>
-                <BuildControls ingredientAdded={this.addIngredientHandler}/>
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls ingredientAdded={this.addIngredientHandler} />
             </Fragment>
         )
     }
